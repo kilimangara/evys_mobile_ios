a12eb12c35f9b8145077c7e79e50242a9af7ded0
nikitazlain <kilimangara@yandex.ru>
2018-04-14 01:43:57 +0300

    Initial Commit

---
+++ evys/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  evys
+//
+//  Created by Nikita Zlain on 14.04.18.
+//  Copyright © 2018 Nikita Zlain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
